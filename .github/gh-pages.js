8c983c92c3425b29de130ee0c291eea74d8787c4
YieldRay <yieldray@outlook.com>
2022-09-18 16:44:22 +0800

    update gh-pages

--- .github/gh-pages.js
+++ .github/gh-pages.js
@@ -1,24 +1,25 @@
 // ! This script is only used for Github Actions
 // ! DO NOT RUN IT LOCALLY
 
-const { opendir, rm, rmdir } = require("fs/promises");
-
-async function ls(path = "./") {
-    const files = [],
-        folders = [];
+/* Config */
+const config = {
+    includeFile: ["index.html"],
+    includeDir: ["dist"],
+};
+/* Program Start */
+const { opendir, rm } = require("fs/promises");
+(async function (config) {
+    const files = [];
+    const dirs = [];
     try {
-        const dir = await opendir(path);
-        for await (const dirent of dir) {
-            if (dirent.isDirectory()) folders.push(dirent.name);
+        const dir = await opendir("./");
+        for await (const dirent of dir)
+            if (dirent.isDirectory()) dirs.push(dirent.name);
             else files.push(dirent.name);
-        }
     } catch (err) {
         console.error(err);
+        process.exit(1);
     }
-    return { folders, files };
-}
-
-ls().then(({ folders, files }) => {
-    folders.filter((f) => f !== "dist").forEach((f) => rmdir(f, { recursive: true }));
-    files.filter((f) => f !== "index.html").forEach((f) => rm(f));
-});
+    dirs.filter((f) => !config.includeDir.find((d) => d === f)).forEach((f) => rm(f, { recursive: true }));
+    files.filter((f) => !config.includeFile.find((d) => d === f)).forEach((f) => rm(f));
+})(config);
