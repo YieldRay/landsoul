7c8c58c71ba33f842ef76b240667ad5885c97e52
YieldRay <yieldray@outlook.com>
2025-09-30 15:57:52 +0800

    fix actions

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+export default {
     plugins: [
         require("autoprefixer"),
         require("postcss-preset-env")({ browsers: "> 0.5%, last 2 versions, Firefox ESR, not dead" }),
